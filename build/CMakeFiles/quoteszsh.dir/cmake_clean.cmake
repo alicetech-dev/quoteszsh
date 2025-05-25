94d50a12684aaa578ef984630700f9fbea30e5b6
alicetech-dev <alice.munozv@correoaiep.cl>
2025-05-25 01:31:42 -0400

    cambios para añadir mas variedad de prompts y se cambia la prioridad en la funcion main para primero llamar a ollama local y luego en caso de no estar disponible el modelo de ollama en local llamar a la api de openrouter.  esto para ahorrar consumo de API remota

---
+++ build/CMakeFiles/quoteszsh.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quoteszsh.dir/link.d"
+  "CMakeFiles/quoteszsh.dir/main.cpp.o"
+  "CMakeFiles/quoteszsh.dir/main.cpp.o.d"
+  "quoteszsh"
+  "quoteszsh.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quoteszsh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/quoteszsh.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for quoteszsh.
+# This may be replaced when dependencies are built.
